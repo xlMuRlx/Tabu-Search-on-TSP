2755133b332af2592485324fc6acaf4cc86e22f6
xlMuRlx <andraz.mur44@gmail.com>
2018-12-30 15:39:25 +0100

    Posodobitev program.py

--- program.py
+++ program.py
@@ -14,6 +14,64 @@ povezave = {1:[[2,3], [3,2], [4,5]],
 
 
 
+def prevedba_koordinat(n, datoteka):
+    kordinate = []
+    for i in range(n):
+        vrstica = []
+        for j in range(n):
+            vrstica.append(0)
+        kordinate.append(vrstica)
+    f = open(datoteka, "r")
+    for i in range(n):
+        kordinate[i] = f.readline().split()
+        for j in range(3):
+            kordinate[i][j] = float(kordinate[i][j])
+    matrika_sosednosti = []
+    for i in range(n):
+        vrstica = []
+        for j in range(n):
+            vrstica.append(0)
+        matrika_sosednosti.append(vrstica)
+    for i in range(n):
+        for j in range(n):
+            matrika_sosednosti[i][j] = (abs(kordinate[i][1]-kordinate[j][1])**2 + abs(kordinate[i][2] - kordinate[j][2])**2)**(1/2)
+    return matrika_sosednosti
+
+
+
+def usmerjena_matrika_slovar(matrika):
+    slovar = {}
+    stevec1 = 1
+    for vrstica in matrika:
+        sosedi = []
+        stevec2 = 1
+        for sosed in vrstica:
+            if (sosed != 0) and (sosed != float('inf')):
+                sosedi.append([stevec2, sosed])
+            stevec2 += 1
+        slovar[stevec1] = sosedi
+        stevec1 += 1
+    return slovar
+
+
+
+def neusmerjena_matrika_slovar(matrika):
+    n = len(matrika)
+    slovar = {}
+    vozlisca = []
+    for i in range(0, n-1):
+        slovar[i] = []
+        vozlisca.append(i)
+    for i in range(0, n-1):
+        for j in range(i, n-1):
+            dolzina = matrika[i][j]
+            if (dolzina != 0) and (dolzina != float('inf')):
+                slovar[i].append([j, dolzina])
+                slovar[j].append([i, dolzina])
+    return slovar, vozlisca
+
+
+
 def dolzina_poti(pot, povezave):
     dolzina = 0
     for i in range(0, len(pot)-1):
@@ -28,40 +86,48 @@ def dolzina_poti(pot, povezave):
     return dolzina
 
 
-
+                
 def poisci_resitev(vozlisca, povezave):
     zacetek = vozlisca[0]
     pot = [zacetek]
     vozlisce = zacetek
-    prepovedani = {}
-    for v in vozlisca:
-        prepovedani[v] = []
+    naslednji_koraki = []
+    stevec = 0 #gleda, kako smo do vozlišča prišli, t.j. bodisi z normalnim
+            #korakom naprej (stevec = 0) bodisi s korakom nazaj (stevec = 1)
     while True:
-        sosedi = []
-        for sosed in povezave[vozlisce]:
-            if (sosed[0] not in pot) and (sosed[0] not in prepovedani[vozlisce]):
-                sosedi.append(sosed[0])
-        if sosedi == []:
+        if stevec == 0:
+            sosedi = []
+            for sosed in povezave[vozlisce]:
+                if sosed[0] not in pot:
+                    sosedi.append(sosed[0])
+            naslednji_koraki.append(sosedi)
+        izbire = naslednji_koraki[-1][:]
+        if izbire == []:
             pot.remove(vozlisce)
+            naslednji_koraki.remove(izbire)
+            naslednji_koraki[-1].remove(vozlisce)
             vozlisce = pot[-1]
+            stevec = 1
         else:
-            random.shuffle(sosedi)
-            novo_vozlisce = sosedi.pop(0)
+            random.shuffle(izbire)
+            novo_vozlisce = izbire.pop(0)
             pot.append(novo_vozlisce)
             if len(pot) == len(vozlisca):
                 koncni_sosedi = []
-                for sosed in povezave[vozlisce]:
+                for sosed in povezave[novo_vozlisce]:
                     koncni_sosedi.append(sosed[0])
                 if zacetek in koncni_sosedi:
                     pot.append(zacetek)
                     return pot
                 else:
-                    pot.remove(vozlisce)
-                    prepovedani[vozlisce].append(novo_vozlisce)
+                    pot.remove(novo_vozlisce)
+                    naslednji_koraki[-1].remove(novo_vozlisce)
                     vozlisce = pot[-1]
+                    stevec = 1
             else:
                 vozlisce = novo_vozlisce
-                
+                stevec = 0
+
 
 
 def okolica_poti(pot, povezave):
@@ -80,29 +146,44 @@ def okolica_poti(pot, povezave):
 
 
 
-# Preveri še primer, če dobiš pot brez okolice!!!
 def tabu_search(vozlisca, povezave):
     najboljsa = poisci_resitev(vozlisca, povezave)
+    naj_dolzina = float('inf')
     pot = najboljsa[:]
     tabu = []
     koraki = 0
-    while koraki < 10000:
-        kandidati = okolica_poti(pot, povezave)
-        dolzina = float('inf')
+    while koraki < 100:
+        kandidati = []
+        for kandidat in okolica_poti(pot, povezave):
+            kandidati.append(kandidat)
+        razlika = []
         for kandidat in kandidati:
             if kandidat not in tabu:
+                razlika.append(kandidat)
+        if razlika == []:
+            pot = poisci_resitev(vozlisca, povezave)
+            koraki += 1
+        else:
+            dolzina = float('inf')
+            for kandidat in razlika:
                 if dolzina_poti(kandidat, povezave) < dolzina:
                     dolzina = dolzina_poti(kandidat, povezave)
                     nova_pot = kandidat[:]
-        if nova_pot not in tabu:
             tabu.append(nova_pot)
-        if len(tabu) > 100:
-            tabu.pop(0)
-        if dolzina_poti(nova_pot, povezave) < dolzina_poti(najboljsa, povezave):
-            najboljsa = nova_pot[:]
-        pot = nova_pot[:]
-        koraki += 1
-    return najboljsa
+            if len(tabu) > 50:
+                tabu.pop(0)
+            nova_dolzina = dolzina_poti(nova_pot, povezave)
+            if nova_dolzina < naj_dolzina:
+                najboljsa = nova_pot[:]
+                naj_dolzina = nova_dolzina
+            pot = nova_pot[:]
+            koraki += 1
+    return (najboljsa, naj_dolzina)
+
+
+
+
+
 
 
 
